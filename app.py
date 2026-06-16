65fd3837ba018a12af5fe1ecdf2e0b743bef7b1a
TSANGNING DJIOGAP GRACE <mappouograce@gmail.com>
2026-06-16 19:40:17 +0100

    feat: add Gradio app and pre-commit black formatter

---
+++ app.py
@@ -0,0 +1,16 @@
+import gradio as gr
+
+
+def hello(name: str) -> str:
+    return f"Hello, {name}!"
+
+
+demo = gr.Interface(
+    fn=hello,
+    inputs=gr.Textbox(label="Ton prénom"),
+    outputs=gr.Textbox(label="Réponse"),
+    title="Hello World - BUNEC MLOps",
+)
+
+if __name__ == "__main__":
+    demo.launch()
